9dcf37db2a003bee0b221ce2db06b680350a6c6d
kinsprite <qxqs11@126.com>
2020-09-17 22:59:36 +0800

    update response msg

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const apm = require('elastic-apm-node').start();
 
 const app = require('express')();
 
-app.get('/',  async function (req, res) {
-
+app.get('/api/:msg', async function (req, res) {
   const resAll = await Promise.all([
-    axios.get('http://apm-nodejs-app-b:8080/api/hello-a-b'),
-    axios.get('http://apm-nodejs-app-c:8080/api/hello-a-c'),
+    axios.get('http://apm-nodejs-app-c:8080/api/hello-b-c-1'),
+    axios.get('http://apm-nodejs-app-c:8080/api/hello-b-c-2'),
   ]);
 
   res.send({
-    a: 'Hello World!',
-    b: resAll[0].data,
-    c: resAll[1].data,
+    b: req.params.msg,
+    c1: resAll[0].data,
+    c2: resAll[1].data,
   });
 });
 
